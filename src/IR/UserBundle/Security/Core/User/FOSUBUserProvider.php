d2506567402c9474223b35d0080919ff1baeaeb2
achinyakov <shvvbro@mail.ru>
2017-06-07 15:55:00 +0300

    Oauth configured

--- src/IR/UserBundle/Security/Core/User/FOSUBUserProvider.php
+++ src/IR/UserBundle/Security/Core/User/FOSUBUserProvider.php
@@ -15,35 +15,10 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class FOSUBUserProvider extends BaseClass
 {
     /**
-     * {@inheritDoc}
-     */
-    public function connect(UserInterface $user, UserResponseInterface $response)
-    {
-        die('dd');
-        $property = $this->getProperty($response);
-        $username = $response->getUsername();
-        //on connect - get the access token and the user ID
-        $service = $response->getResourceOwner()->getName();
-        $setter = 'set'.ucfirst($service);
-        $setter_id = $setter.'Id';
-        $setter_token = $setter.'AccessToken';
-        //we "disconnect" previously connected users
-        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
-            $previousUser->$setter_id(null);
-            $previousUser->$setter_token(null);
-            $this->userManager->updateUser($previousUser);
-        }
-        //we connect current user
-        $user->$setter_id($username);
-        $user->$setter_token($response->getAccessToken());
-        $this->userManager->updateUser($user);
-    }
-    /**
      * {@inheritdoc}
      */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
-         die('dd');
         $username = $response->getUsername();
         $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
         //when the user is registrating
